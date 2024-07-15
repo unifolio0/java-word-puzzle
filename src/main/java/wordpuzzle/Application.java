6338d85872751b4384a0d3cdf52d7e9afcd5f902
임수빈 <robin980108@naver.com>
2024-07-15 15:51:45 +0900

    초기 세팅

---
+++ src/main/java/wordpuzzle/Application.java
@@ -0,0 +1,7 @@
+package wordpuzzle;
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+    }
+}
---
+++ src/test/java/wordpuzzle/ApplicationTest.java
@@ -0,0 +1,7 @@
+package wordpuzzle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+}
